92213124025adac5461f4fdc55dc2fd7b6e302d0
NikaEG <ally.via@gmail.com>
2020-07-21 15:50:05 +0000

    You choose how long it is

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #loop to repeat "for" loops and add +1
 
-for j in range(1, 6):
+length = int(input("How long would you like the stack to be \n"))
+for j in range(1, length+1):
   for i in range(1, j+1):
    print(i, end = " ")
   print()
